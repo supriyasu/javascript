0f5201eaccac9b6e02b9e9ac03fd311cef3e81eb
Supriya Kesharwani <kesharwani.supriya12@gmail.com>
2025-01-01 20:01:25 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3) // code readability should be high
+
+console.log("VS");
+
+
+let name= "VS"
+let age= 28
+let isLoggedIn= false
+let state;
+// primitive data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // representation of empty value
+// undefined => value not defined
+// symbol => uniqueness of component
+
+
+// object => non-primitive data type
+
+console.log(typeof undefined); //undefined is undefined type
+console.log(typeof null); //null is object type
+
+
