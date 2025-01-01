9922271480e2e0ec72e949851550b49debfe36dd
Supriya Kesharwani <kesharwani.supriya12@gmail.com>
2025-01-01 19:38:40 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId=144553
+let accountEmail="supriya.skk@gmail.com"
+var accountPassword="12345"
+accountCity="Deosar"
+let accountState;
+
+// accountId=2 //const can't be changed
+
+
+accountEmail="ss@gmail.com"
+accountPassword=212121
+accountCity="Bangalore"
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issue in block space and funtional space
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
