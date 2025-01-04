848ebc8d0dc7edece5484cf32f18263e35851ae0
Supriya Kesharwani <kesharwani.supriya12@gmail.com>
2025-01-04 19:54:48 +0000

    Comparsion Commit

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// node 01_basics/04_comparison.js
+
+// console.log(2 > 1);
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.table([null > 0 , null == 0 , null >=0]);
+
+// console.table([undefined > 0 , undefined == 0 , undefined >=0]);
+// avoid above codes for code writing
+
+// comparison & equality check is different in JS
+
+// Strict check === value with datatype check
+
+console.log("2" === 2);
+
+
