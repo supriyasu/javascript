0325bc1e2b48b7d9203d8307dc5bf857e7f59a62
Supriya Kesharwani <kesharwani.supriya12@gmail.com>
2025-01-07 18:24:07 +0000

    commit all

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -17,7 +17,7 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-// console.log(id === anotherId);
+console.log(id === anotherId);
 // console.log(id);
 // console.log(anotherId);
 
@@ -44,15 +44,15 @@ let myObj ={
 }
 
 const myFunction = function(){
-    // console.log("Hello World!");
+    console.log("Hello World!");
     return "Success"
 }
 myFunction();
 // console.log( typeof bigNumber);
 
-// console.log(myFunction());
+console.log(myFunction());
 
-// console.log(typeof myFunction());
+console.log(typeof myFunction());
 
 
 // ****** Memory *****
