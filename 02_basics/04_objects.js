c8fe5037090def31d59834fe59adc953c5cb9434
Supriya Kesharwani <kesharwani.supriya12@gmail.com>
2025-01-14 19:22:59 +0000

    Object2 commit

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,80 @@
+// node 02_basics/04_objects.js
+
+// Singleton Object
+
+// const tinderUser = new Object()
+
+// Non- singleton object
+
+let tinderUser={}
+tinderUser.id = "123vs"
+tinderUser.name = "Suvika"
+tinderUser.isLoggedIn = false
+
+tinderUser.name = "Suvika1"
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "suvika@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "VS",
+            lastname: "KG"
+        }
+    }
+}
+
+// console.log(regularUser.fullname?.userfullname.firstname);
+
+const obj1 ={1: "a", 2: "b"}
+const obj2 ={3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2 }
+// const obj3 = Object.assign(obj1,obj2)
+// const obj3 = Object.assign({},obj1,obj2)  // {} => treat as target => optional value => empty object
+
+// most useful =>spread
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// Assignment of multiple objects in array
+const users = [
+    { 
+        id : 1,
+        email : "vs@gmail.com"
+    },
+    { 
+        id : 1,
+        email : "vs@gmail.com"
+    },
+    { 
+        id : 1,
+        email : "vs@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser); // [ [ 'id', '123vs' ], [ 'name', 'Suvika1' ], [ 'isLoggedIn', false ] ]
+
+// ***** Return keys of object in the form of Array
+console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)); // [ '123vs', 'Suvika1', false ]
+
+// Return object in form of Array as Key-value pair
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123vs' ], [ 'name', 'Suvika1' ], [ 'isLoggedIn', false ] ]
+
+// hasOwnProperty - to check attribute of object whether it's present or not
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
+
+console.log(tinderUser.hasOwnProperty('isLogged')); // false
+
+
+
+
+
+
+
+
+
+
